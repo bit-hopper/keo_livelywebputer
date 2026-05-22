de54be94d2ecc439cc09d0a4b03ef7e30475a6d8
hgfx16 <dreamscheme@proton.me>
2026-05-21 19:31:28 -0700

    fix relative path

--- core/lively/store/Interface.js
+++ core/lively/store/Interface.js
@@ -1,143 +1,178 @@
-module('lively.store.Interface').requires('lively.Network').toRun(function() {
-
-Object.extend(lively.store, {
-    cachedStorageAccessors: {},
-    get: function(spec) {
+module("lively.store.Interface")
+  .requires("lively.Network")
+  .toRun(function () {
+    Object.extend(lively.store, {
+      cachedStorageAccessors: {},
+      get: function (spec) {
         if (this.cachedStorageAccessors[spec.id]) {
-            return this.cachedStorageAccessors[spec.id];
+          return this.cachedStorageAccessors[spec.id];
         }
         if (!spec.type) {
-            throw new Error('Storage type must be specified!');
+          throw new Error("Storage type must be specified!");
         }
         var storageClass = lively.store[spec.type];
         if (!storageClass) {
-            throw new Error('No such storage type ' + spec.type);
+          throw new Error("No such storage type " + spec.type);
         }
         var storage = new storageClass(spec);
-        return this.cachedStorageAccessors[spec.id] = storage;
-    },
+        return (this.cachedStorageAccessors[spec.id] = storage);
+      },
 
-    flushCache: function() {
+      flushCache: function () {
         this.cachedStorageAccessors = {};
-    }
-});
+      },
+    });
 
-Object.subclass('lively.store.ObjectStorage',
-'initializing', {
-    initialize: function(spec) {
-        this.stored = {}
-    }
-},
-'accessing', {
-    set: function(key, value) {
-        this.stored[key] = value;
-    },
+    Object.subclass(
+      "lively.store.ObjectStorage",
+      "initializing",
+      {
+        initialize: function (spec) {
+          this.stored = {};
+        },
+      },
+      "accessing",
+      {
+        set: function (key, value) {
+          this.stored[key] = value;
+        },
 
-    get: function(key) {
-        return this.stored[key];
-    }
-});
+        get: function (key) {
+          return this.stored[key];
+        },
+      },
+    );
 
-Object.subclass('lively.store.FileStorage',
-'initializing', {
-    initialize: function(spec) {
-        this.url = spec.url;
-    }
-},
-'accessing', {
-    set: function(data) {
-        var webR = new WebResource(this.url);
-        webR.put(data);
-    },
+    Object.subclass(
+      "lively.store.FileStorage",
+      "initializing",
+      {
+        initialize: function (spec) {
+          this.url = spec.url;
+        },
+      },
+      "accessing",
+      {
+        set: function (data) {
+          var webR = new WebResource(this.url);
+          webR.put(data);
+        },
 
-    get: function(optCb) {
-        var webR = new WebResource(this.url),
+        get: function (optCb) {
+          var webR = new WebResource(this.url),
             result = webR.get().content;
-        if (optCb) optCb(null, result);
-        return result;
-    }
-});
+          if (optCb) optCb(null, result);
+          return result;
+        },
+      },
+    );
 
-Object.subclass('lively.store.CouchDBStorage',
-'initializing', {
-    initialize: function(spec) {
-        var reqPath = 'couchdb/' + spec.id + '/'; // id = db name
-        this.couchdbURL = URL.nodejsBase.withFilename(reqPath);
-    }
-},
-'accessing', {
-    set: function(key, value) {
-        var webR = this.couchdbURL.withFilename(key).asWebResource();
-        webR.put(JSON.stringify(value), 'application/json');
-    },
+    Object.subclass(
+      "lively.store.CouchDBStorage",
+      "initializing",
+      {
+        initialize: function (spec) {
+          var reqPath = "couchdb/" + spec.id + "/"; // id = db name
+          this.couchdbURL = URL.nodejsBase.withFilename(reqPath);
+        },
+      },
+      "accessing",
+      {
+        set: function (key, value) {
+          var webR = this.couchdbURL.withFilename(key).asWebResource();
+          webR.put(JSON.stringify(value), "application/json");
+        },
 
-    get: function(key, optCb) {
-        var webR = this.couchdbURL.withFilename(key).asWebResource(),
+        get: function (key, optCb) {
+          var webR = this.couchdbURL.withFilename(key).asWebResource(),
             data = webR.get().content;
-        try { return JSON.parse(data); } catch(e) { return {error: e}}
-    },
+          try {
+            return JSON.parse(data);
+          } catch (e) {
+            return { error: e };
+          }
+        },
 
-    ensureExistance: function() {
-        this.couchdbURL.asWebResource().put();
-    },
+        ensureExistance: function () {
+          this.couchdbURL.asWebResource().put();
+        },
 
-    remove: function() {
-        this.couchdbURL.asWebResource().del();
-    }
-});
+        remove: function () {
+          this.couchdbURL.asWebResource().del();
+        },
+      },
+    );
 
-Object.subclass("lively.store.ObjectRepository",
-"initializing", {
-
-    initialize: function (url) {
-        this.repoURL = url ? url : URL.root;
-    }
-
-},
-"accessing", {
-
-    getServerInterfaceURL: function() {
-        return this.repoURL.withPath(URL.nodejsBase.fullPath() + "ObjectRepositoryServer/");
-    }
-
-},
-"querying", {
-
-    getRecords: function (querySpec, thenDo)  {
-        // new lively.store.ObjectRepository().getRecords()
-        var res = this.getServerInterfaceURL()
-            .withQuery({getRecords: encodeURIComponent(JSON.stringify(querySpec))})
-            .asWebResource().noProxy();
-        if (thenDo != null) {
-            res.withJSONWhenDone(function(json, status) {
+    Object.subclass(
+      "lively.store.ObjectRepository",
+      "initializing",
+      {
+        initialize: function (url) {
+          this.repoURL = url ? url : URL.root;
+        },
+      },
+      "accessing",
+      {
+        getServerInterfaceURL: function () {
+          return this.repoURL.withPath(
+            URL.nodejsBase.fullPath() + "ObjectRepositoryServer/",
+          );
+        },
+      },
+      "querying",
+      {
+        getRecords: function (querySpec, thenDo) {
+          // new lively.store.ObjectRepository().getRecords()
+          var res = this.getServerInterfaceURL()
+            .withQuery({
+              getRecords: encodeURIComponent(JSON.stringify(querySpec)),
+            })
+            .asWebResource()
+            .noProxy();
+          if (thenDo != null) {
+            res
+              .withJSONWhenDone(function (json, status) {
                 // Handle empty results gracefully - versioning may be disabled
-                if (JSON.prettyPrint(json) == "[]"){
-                    json = []; // Use empty array instead of throwing
-                } 
-                thenDo(status.isSuccess() ? null : status, json); }).beAsync().get();
+                // FIX attribution Connection such that published objects have verifiable identity/DID - this is just a stub comment
+                if (JSON.prettyPrint(json) == "[]") {
+                  json = []; // Use empty array instead of throwing
+                }
+                thenDo(status.isSuccess() ? null : status, json);
+              })
+              .beAsync()
+              .get();
             return this;
-        } else {
+          } else {
             var content = res.beSync().get().content;
-            if (!res.status.isSuccess())
-                throw new Error(content);
+            if (!res.status.isSuccess()) throw new Error(content);
             var json;
-            try { json = JSON.parse(content); } catch(e) { json = {error: e} }
+            try {
+              json = JSON.parse(content);
+            } catch (e) {
+              json = { error: e };
+            }
             return json;
-        }
-    },
+          }
+        },
 
-    diff: function(querySpecA, querySpecB, options, thenDo)  {
-        options = options || {};
-        this.getServerInterfaceURL().withFilename('diff').withQuery({
-            getRecordsA: JSON.stringify(querySpecA),
-            getRecordsB: JSON.stringify(querySpecB),
-            isJSON: !!options.isJSON,
-            isLivelyWorld: !!options.isLivelyWorld
-        }).asWebResource().withJSONWhenDone(function(json, status) {
-             thenDo(status.isSuccess() ? null : status, json); }).beAsync().get();
-        return this;
-    }
-
-});
-
-}); // end of module
+        diff: function (querySpecA, querySpecB, options, thenDo) {
+          options = options || {};
+          this.getServerInterfaceURL()
+            .withFilename("diff")
+            .withQuery({
+              getRecordsA: JSON.stringify(querySpecA),
+              getRecordsB: JSON.stringify(querySpecB),
+              isJSON: !!options.isJSON,
+              isLivelyWorld: !!options.isLivelyWorld,
+            })
+            .asWebResource()
+            .withJSONWhenDone(function (json, status) {
+              thenDo(status.isSuccess() ? null : status, json);
+            })
+            .beAsync()
+            .get();
+          return this;
+        },
+      },
+    );
+  }); // end of module
--- core/servers/OptimizedLoadingServer.js
+++ core/servers/OptimizedLoadingServer.js
@@ -1,40 +1,45 @@
 /*global require, process, __dirname*/
 /*global require, process, __dirname*/
 
-var path             = require("path"),
-    util             = require("util"),
-    fs               = require("fs"),
-    zlib             = require('zlib'),
-    crypto           = require("crypto"),
-    concat           = require("source-map-concat"),
-    babel            = require("babel-core"),
-    lang             = require("lively.lang"),
-    rootDir          = process.env.WORKSPACE_LK || path.resolve(__dirname, '../..'),
-    relWorkDir       = ".optimized-loading-cache",
-    workDir          = path.join(rootDir, relWorkDir),
-    combinedFile     = path.join(workDir, "combined.js"),
-    combinedHashFile = path.join(workDir, "combined.js.hash"),
-    _combinedFileAndHashCached = null,
-    _combinedFileAndHashCachedTimeout = 1000/*ms*/;
-
+var path = require("path"),
+  util = require("util"),
+  fs = require("fs"),
+  zlib = require("zlib"),
+  crypto = require("crypto"),
+  concat = require("source-map-concat"),
+  babel = require("babel-core"),
+  lang = require("lively.lang"),
+  rootDir = process.env.WORKSPACE_LK || path.resolve(__dirname, "../.."),
+  relWorkDir = ".optimized-loading-cache",
+  workDir = path.join(rootDir, relWorkDir),
+  combinedFile = path.join(workDir, "combined.js"),
+  combinedHashFile = path.join(workDir, "combined.js.hash"),
+  _combinedFileAndHashCached = null,
+  _combinedFileAndHashCachedTimeout = 1000; /*ms*/
 
 (function onStartup() {
   combinedFileAndHash()
     .then(() => console.log("optimized-loading-cache initialized"))
-    .catch(err => console.error("optimized-loading-cache initialization errored", err))
+    .catch((err) =>
+      console.error("optimized-loading-cache initialization errored", err),
+    );
 })();
 
 function combinedFileAndHashCached() {
   // for fast subsequent requests cache the hash / combined result
   // combinedFileAndHashCached().then(x => console.log(x)).catch(err => console.error(err))
-  if (!Config.optimizedLoading) return Promise.reject("Optimized loading not enabled");
-  return _combinedFileAndHashCached ?
-    Promise.resolve(_combinedFileAndHashCached) :
-    combinedFileAndHash().then(result => {
-      _combinedFileAndHashCached = result;
-      setTimeout(() => _combinedFileAndHashCached = null, _combinedFileAndHashCachedTimeout);
-      return result;
-    });
+  if (!Config.optimizedLoading)
+    return Promise.reject("Optimized loading not enabled");
+  return _combinedFileAndHashCached
+    ? Promise.resolve(_combinedFileAndHashCached)
+    : combinedFileAndHash().then((result) => {
+        _combinedFileAndHashCached = result;
+        setTimeout(
+          () => (_combinedFileAndHashCached = null),
+          _combinedFileAndHashCachedTimeout,
+        );
+        return result;
+      });
 }
 
 function combinedFileAndHash() {
@@ -49,15 +54,18 @@ function combinedFileAndHash() {
   // combinedFileAndHash().then(x => console.log(x)).catch(err => console.error(err))
   var files = coreFiles(process.env.WORKSPACE_LK);
   return lang.promise.chain([
-    () => lang.promise(fs.mkdir)(workDir).catch(_ => {}),
+    () =>
+      lang
+        .promise(fs.mkdir)(workDir)
+        .catch((_) => {}),
     () => concatAndWrite(files, rootDir, workDir, combinedFile, new Date()),
-    (concatResult) => concatResult.wasChanged ?
-                        computeHash(combinedFile) :
-                        String(fs.readFileSync(combinedHashFile)),
-    (hash) => ({hash: hash, file: combinedFile})
+    (concatResult) =>
+      concatResult.wasChanged
+        ? computeHash(combinedFile)
+        : String(fs.readFileSync(combinedHashFile)),
+    (hash) => ({ hash: hash, file: combinedFile }),
   ]);
 }
-
 
 function prepareFileForConcat(rootDir, cacheDir, file) {
   // es5 translation of core files, stores into cache dir
@@ -66,29 +74,32 @@ function prepareFileForConcat(rootDir, cacheDir, file) {
   // x.then((arg) => console.log(arg))
 
   return new Promise((resolve, _reject) => {
-    var babelExceptions = ['core/lib/lively-libs-debug.js'],
-        cacheFile = path.join(cacheDir, file.replace(/\//g, "_")),
-        mtimeFile = cacheFile + ".mtime",
-        mtime = String(fs.statSync(file).mtime),
-        needsUpdate = !fs.existsSync(cacheFile)
-                   || !fs.existsSync(mtimeFile)
-                   || mtime !== String(fs.readFileSync(mtimeFile)),
-        source;
+    var babelExceptions = ["core/lib/lively-libs-debug.js"],
+      cacheFile = path.join(cacheDir, file.replace(/\//g, "_")),
+      mtimeFile = cacheFile + ".mtime",
+      mtime = String(fs.statSync(file).mtime),
+      needsUpdate =
+        !fs.existsSync(cacheFile) ||
+        !fs.existsSync(mtimeFile) ||
+        mtime !== String(fs.readFileSync(mtimeFile)),
+      source;
 
     if (needsUpdate) {
       console.log(file + " needs update");
-      source = babelExceptions.indexOf(file) > -1 ?
-        fs.readFileSync(file) :
-        babel.transformFileSync(file, {"presets": ["es2015"]}).code;
+      source =
+        babelExceptions.indexOf(file) > -1
+          ? fs.readFileSync(file)
+          : babel.transformFileSync(file, { presets: ["es2015"] }).code;
       fs.writeFileSync(cacheFile, source);
       fs.writeFileSync(mtimeFile, mtime);
     }
 
     resolve({
-      source: file, code: source && String(source),
+      source: file,
+      code: source && String(source),
       sourcesRelativeTo: rootDir.replace(/\/$/, "/"),
       cacheFile: cacheFile,
-      wasChanged: !!source
+      wasChanged: !!source,
     });
   });
 }
@@ -102,110 +113,142 @@ function concatAndWrite(files, rootDir, workDir, targetFilePath, time) {
   // concat needs spec per file like
   // {source: pathToFile, code: STRING, sourcesRelativeTo: STRING}
 
-  return Promise.all(files.map(f => prepareFileForConcat(rootDir, workDir, f)))
-    .then((filesForConcat) => {
-      var changed = filesForConcat.some(ea => ea.wasChanged);
-      if (!changed && fs.existsSync(combinedFile) && fs.existsSync(combinedHashFile)) {
-        return {file: combinedFile, wasChanged: false};
-      }
+  return Promise.all(
+    files.map((f) => prepareFileForConcat(rootDir, workDir, f)),
+  ).then((filesForConcat) => {
+    var changed = filesForConcat.some((ea) => ea.wasChanged);
+    if (
+      !changed &&
+      fs.existsSync(combinedFile) &&
+      fs.existsSync(combinedHashFile)
+    ) {
+      return { file: combinedFile, wasChanged: false };
+    }
 
-      return lang.promise.chain([
-        () => Promise.all(filesForConcat.map(ea => ea.code ? ea : lang.obj.merge(ea, {code: String(fs.readFileSync(ea.cacheFile))}))),
-        (filesForConcat) => concat(filesForConcat, {delimiter: "\n", mapPath: targetFilePath + ".jsm"}),
-        (concatenated) => {
-          concatenated.prepend(createHeader(time, files));
-          var result = concatenated.toStringWithSourceMap({file: path.basename(targetFilePath)});
-          return lang.promise(fs.writeFile)(targetFilePath, result.code);
-        },
-        // () => console.log(targetFilePath + " written"),
-        () => ({file: targetFilePath, wasChanged: true})
-      ]);
-    });
+    return lang.promise.chain([
+      () =>
+        Promise.all(
+          filesForConcat.map((ea) =>
+            ea.code
+              ? ea
+              : lang.obj.merge(ea, {
+                  code: String(fs.readFileSync(ea.cacheFile)),
+                }),
+          ),
+        ),
+      (filesForConcat) =>
+        concat(filesForConcat, {
+          delimiter: "\n",
+          mapPath: targetFilePath + ".jsm",
+        }),
+      (concatenated) => {
+        concatenated.prepend(createHeader(time, files));
+        var result = concatenated.toStringWithSourceMap({
+          file: path.basename(targetFilePath),
+        });
+        return lang.promise(fs.writeFile)(targetFilePath, result.code);
+      },
+      // () => console.log(targetFilePath + " written"),
+      () => ({ file: targetFilePath, wasChanged: true }),
+    ]);
+  });
 }
 
 function createHeader(timestamp, files) {
   // Note: JSLoader relies on JSLoader.expectToLoadModules to be initialized!
-  return util.format('// This file was generated on %s\n\n'
-                   + 'JSLoader.expectToLoadModules(%s);\n\n',
-                     timestamp.toGMTString(),
-                     JSON.stringify(files));
+  return util.format(
+    "// This file was generated on %s\n\n" +
+      "JSLoader.expectToLoadModules(%s);\n\n",
+    timestamp.toGMTString(),
+    JSON.stringify(files),
+  );
 }
-
 
 function computeHash(combinedFile) {
   return new Promise((resolve, reject) => {
-    var md5sum = crypto.createHash('md5'), hash;
-    md5sum.setEncoding('hex');
-    md5sum.on('data', (d) => hash = String(d));
-    md5sum.on('error', (err) => reject(err));
-    md5sum.on('end', () => resolve(hash));
+    var md5sum = crypto.createHash("md5"),
+      hash;
+    md5sum.setEncoding("hex");
+    md5sum.on("data", (d) => (hash = String(d)));
+    md5sum.on("error", (err) => reject(err));
+    md5sum.on("end", () => resolve(hash));
     fs.createReadStream(combinedFile).pipe(md5sum);
-  }).then(hash => {
+  }).then((hash) => {
     fs.writeFileSync(combinedHashFile, hash);
     return hash;
-  })
+  });
 }
 
 function coreFiles(baseDir) {
   var cfg = lively.Config,
-      libsFile = path.join(baseDir, 'core/lib/lively-libs-debug.js'),
-      // Convert bootstrap files to absolute paths by resolving them relative to baseDir
-      bootstrapFiles = (cfg.get("bootstrapFiles") || []).map(function(file) {
-        return path.join(baseDir, file);
-      }),
-      modulesToInclude = cfg.get("bootstrapModules")
-        .concat(cfg.get("modulesBeforeWorldLoad"))
-        .concat(cfg.get("modulesOnWorldLoad"));
+    libsFile = path.join(baseDir, "core/lib/lively-libs-debug.js"),
+    // Convert bootstrap files to absolute paths by resolving them relative to baseDir
+    bootstrapFiles = (cfg.get("bootstrapFiles") || []).map(function (file) {
+      return path.join(baseDir, file);
+    }),
+    modulesToInclude = cfg
+      .get("bootstrapModules")
+      .concat(cfg.get("modulesBeforeWorldLoad"))
+      .concat(cfg.get("modulesOnWorldLoad"));
 
-  var coreFiles = spliceInDependencies(modulesToInclude.map(moduleToFile).reverse());
+  var coreFiles = spliceInDependencies(
+    modulesToInclude.map(moduleToFile).reverse(),
+  );
   coreFiles = [libsFile].concat(bootstrapFiles).concat(coreFiles);
-  
+
   // Convert all absolute paths back to relative paths for the module loading system
-  coreFiles = coreFiles.map(function(file) {
-    return file[0] === "/" || (file.length > 1 && file[1] === ":") ? path.relative(baseDir, file) : file;
+  coreFiles = coreFiles.map(function (file) {
+    return file[0] === "/" || (file.length > 1 && file[1] === ":")
+      ? path.relative(baseDir, file)
+      : file;
   });
-  
+
   return coreFiles;
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
   function moduleToFile(module) {
     // TODO: Adapt module load logic
-    var relFile = 'core/' + module.replace(/\./g, '/') + '.js',
-        absFile = path.join(baseDir, relFile);
+    var relFile = "core/" + module.replace(/\./g, "/") + ".js",
+      absFile = path.join(baseDir, relFile);
     if (fs.existsSync(absFile)) return absFile;
-    relFile = module.replace(/\./g, '/') + '.js';
+    relFile = module.replace(/\./g, "/") + ".js";
     absFile = path.join(baseDir, relFile);
     return absFile;
   }
 
   function spliceInDependencies(files) {
     // rk 2014-10-25: Uuhhh ha, this looks like an ad-hoc parsing adventure...
-    var i = 0, dependencies = {};
+    var i = 0,
+      dependencies = {};
     while (i < files.length) {
-        var filename = files[i];
-        if (dependencies[filename]) {
-            dependencies[filename].forEach(function(dep) { files.splice(i + 1, 0, dep); });
-            i++;
-            continue;
-        }
-        dependencies[filename] = [];
-        try {
-            var content = fs.readFileSync(filename).toString();
-            // FIXME: do real parsing, evil eval
-            var modRegEx = /module\((.*?)\)\.requires\((.*?)\)./g;
-            var moduleDefs = modRegEx.exec(content);
-            if (moduleDefs) {
-                var req = eval('[' + moduleDefs[2] + ']');
-                var deps = dependencies[filename];
-                req.forEach(function(module) {
-                    var filename = moduleToFile(module);
-                    files.splice(i + 1, 0, filename);
-                    deps.push(filename);
-                });
-            }
-        } catch (e) { console.log('Problems processing: ' + filename); }
+      var filename = files[i];
+      if (dependencies[filename]) {
+        dependencies[filename].forEach(function (dep) {
+          files.splice(i + 1, 0, dep);
+        });
         i++;
+        continue;
+      }
+      dependencies[filename] = [];
+      try {
+        var content = fs.readFileSync(filename).toString();
+        // FIXME: do real parsing, evil eval
+        var modRegEx = /module\((.*?)\)\.requires\((.*?)\)./g;
+        var moduleDefs = modRegEx.exec(content);
+        if (moduleDefs) {
+          var req = eval("[" + moduleDefs[2] + "]");
+          var deps = dependencies[filename];
+          req.forEach(function (module) {
+            var filename = moduleToFile(module);
+            files.splice(i + 1, 0, filename);
+            deps.push(filename);
+          });
+        }
+      } catch (e) {
+        console.log("Problems processing: " + filename);
+      }
+      i++;
     }
 
     // remove duplicates - keep as absolute paths for now, will be converted to relative in coreFiles()
@@ -213,57 +256,60 @@ function coreFiles(baseDir) {
   }
 }
 
-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-
-module.exports = function(_route, app) {
-
-    app.get('/generated/combinedModulesHash.txt', function(_req, res) {
-      combinedFileAndHashCached()
-        .then(hashAndFile => {
-          res.writeHead(200, {
-              'Content-Type': 'text/plain',
-              'Cache-Control': 'no-cache, no-store, must-revalidate',
-              'Pragma': 'no-cache',
-              'Expires': '0'
-          });
-          res.end(hashAndFile.hash);
-        })
-        .catch(err => {
-          res.status(500).end(String('optimized loading error ' + err.stack || err));
+module.exports = function (_route, app) {
+  app.get("/generated/combinedModulesHash.txt", function (_req, res) {
+    combinedFileAndHashCached()
+      .then((hashAndFile) => {
+        res.writeHead(200, {
+          "Content-Type": "text/plain",
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
         });
-    });
+        res.end(hashAndFile.hash);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .end(String("optimized loading error " + err.stack || err));
+      });
+  });
 
-    app.get('/generated/:hash/combinedModules.js', function(req, res) {
+  app.get("/generated/:hash/combinedModules.js", function (req, res) {
+    combinedFileAndHashCached()
+      .then((hashAndFile) => {
+        if (req.headers["if-none-match"] === hashAndFile.hash) {
+          res.status(304);
+          res.end();
+          return;
+        }
 
-      combinedFileAndHashCached()
-        .then(hashAndFile => {
-
-          if (req.headers['if-none-match'] === hashAndFile.hash) {
-            res.status(304); res.end(); return;
-          }
-
-          var stream = fs.createReadStream(hashAndFile.file),
-              oneYear = 1000*60*60*24*30*12,
-              acceptEncoding = req.headers['accept-encoding'] || '',
-              header = {
-                  'Content-Type': 'application/javascript',
-                  'Expires': new Date(Date.now() + oneYear).toGMTString(),
-                  "Cache-Control": "public",
-                  'ETag': hashAndFile.hash
-              };
-          if (acceptEncoding.match(/\bdeflate\b/)) {
-              header['content-encoding'] = 'deflate';
-              stream = stream.pipe(zlib.createDeflate());
-          } else if (acceptEncoding.match(/\bgzip\b/)) {
-              header['content-encoding'] = 'gzip';
-              stream = stream.pipe(zlib.createGzip());
-          }
-          res.writeHead(200, header);
-          stream.pipe(res);
-        })
-        .catch(err => res.status(500).end(String('optimized loading error ' + err.stack || err)));
-    });
-}
+        var stream = fs.createReadStream(hashAndFile.file),
+          oneYear = 1000 * 60 * 60 * 24 * 30 * 12,
+          acceptEncoding = req.headers["accept-encoding"] || "",
+          header = {
+            "Content-Type": "application/javascript",
+            Expires: new Date(Date.now() + oneYear).toGMTString(),
+            "Cache-Control": "public",
+            ETag: hashAndFile.hash,
+          };
+        if (acceptEncoding.match(/\bdeflate\b/)) {
+          header["content-encoding"] = "deflate";
+          stream = stream.pipe(zlib.createDeflate());
+        } else if (acceptEncoding.match(/\bgzip\b/)) {
+          header["content-encoding"] = "gzip";
+          stream = stream.pipe(zlib.createGzip());
+        }
+        res.writeHead(200, header);
+        stream.pipe(res);
+      })
+      .catch((err) =>
+        res
+          .status(500)
+          .end(String("optimized loading error " + err.stack || err)),
+      );
+  });
+};
